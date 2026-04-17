81292b6b45c2216db9a4f0f2f9c4b743081b4814
wtaype <wilder.taype16@gmail.com>
2026-04-16 22:38:24 -0500

    Actualizacion Principal v10.10.10

--- src/smiles/wii.ts
+++ src/smiles/wii.ts
@@ -7,14 +7,14 @@ export const by = "Wilder Taype";
 export const linkme = "https://wtaype.me";
 export const linkweb = "https://amorwii.vercel.app";
 export const ipdev = process.env.NEXT_PUBLIC_MIDEV ?? "";
-export const version = "v10";
+export const version = "v11";
 
 /** Actualizar main luego esto, pero si es mucho, solo esto. (1)
-git tag v10 -m "Version v10" ; git push origin v10  
+git tag v11 -m "Version v11" ; git push origin v11  
 
 //  ACTUALIZACIÓN PRINCIPAL ONE DEV [START] (2)
-git add . ; git commit -m "Actualizacion Principal v10.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v11.10.10" ; git push origin main
 
 // En caso de emergencia, para actualizar el Tag existente. (3)
-git tag -d v10 ; git tag v10 -m "Version v10 actualizada" ; git push origin v10 --force
+git tag -d v11 ; git tag v11 -m "Version v11 actualizada" ; git push origin v11 --force
  ACTUALIZACION TAG[END]  */
