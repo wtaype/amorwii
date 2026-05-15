4ac2078ed1f189f14a2f3f659e812415b15e48dd
wtaype <wilder.taype16@gmail.com>
2026-05-15 13:48:55 -0500

    Actualizacion Principal v16.10.13

---
+++ src/app/wii.ts
@@ -0,0 +1,24 @@
+// INFORMACIÓN DEL APP 
+export const id = 'amorwii'
+export const app = 'AmorWii'
+export const icon = 'fa-heartbeat'
+export const titulo = 'Mensajes de Amor Personalizados para Parejas';
+export const keywii = 'amor, mensajes de amor, cartas de amor, san valentín, aniversarios, amorwii';
+export const descri = 'Crea mensajes de amor personalizados para San Valentín, aniversarios y fechas especiales. Dedicatorias románticas, cartas de amor.';
+export const linkweb = 'https://amorwii.com'; // Sin slash (/), al final
+export const lanzamiento = 2026;
+export const by = 'Wilder Taype';
+export const linkme = 'https://wtaype.github.io/';
+export const ipdev = process.env.NEXT_PUBLIC_MIDEV;
+export const version = "v16";
+
+/** Actualizar main luego esto, pero si es mucho, solo esto. (1)
+git tag v16 -m "Version v16" ; git push origin v16  
+
+//  ACTUALIZACIÓN PRINCIPAL ONE DEV [START] (2)
+git add . ; git commit -m "Actualizacion Principal v16.10.10" ; git push origin main
+
+// En caso de emergencia, para actualizar el Tag existente. (3)
+git tag -d v16 ; git tag v16 -m "Version v16 actualizada" ; git push origin v16 --force
+
+ ACTUALIZACION TAG[END]  */
