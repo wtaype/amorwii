35bc9aa37704e7407efcc745199630104fc039a8
wtaype <wilder.taype16@gmail.com>
2026-05-19 18:16:47 -0500

    Actualizacion Principal v29.10.14

--- src/app/wii.ts
+++ src/app/wii.ts
@@ -11,15 +11,15 @@ export const lanzamiento = 2026;
 export const by = 'Wilder Taype';
 export const linkme = 'https://wtaype.github.io/';
 export const ipdev = process.env.NEXT_PUBLIC_MIDEV;
-export const version = "v29";
+export const version = "v30";
 
 /** Actualizar main luego esto, pero si es mucho, solo esto. (1)
-git tag v29 -m "Version v29" ; git push origin v29  
+git tag v30 -m "Version v30" ; git push origin v30  
 
 //  ACTUALIZACIÓN PRINCIPAL ONE DEV [START] (2)
-git add . ; git commit -m "Actualizacion Principal v29.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v30.10.10" ; git push origin main
 
 // En caso de emergencia, para actualizar el Tag existente. (3)
-git tag -d v29 ; git tag v29 -m "Version v29 actualizada" ; git push origin v29 --force
+git tag -d v30 ; git tag v30 -m "Version v30 actualizada" ; git push origin v30 --force
 
  ACTUALIZACION TAG[END]  */
